2b4fbf65b9991ce6ad8158b157b1868d015669b9
Bernat Brunet <bernat@nan-tic.com>
2025-03-27 01:05:34 +0100

    Add the hours field in address too, to have the possibility of have differents hours depending on addresses. And set it on the sale.
    Task: #176163

--- sale.py
+++ sale.py
@@ -8,8 +8,18 @@ class Sale(metaclass=PoolMeta):
     __name__ = 'sale.sale'
     business_hours = fields.Char('Business Hours')
 
-    @fields.depends('party')
+    @fields.depends('party', 'business_hours', 'shipment_address')
+    def set_business_hours(self):
+        if self.shipment_address and self.shipment_address.business_hours:
+            self.business_hours = self.shipment_address.business_hours
+        elif self.party and not self.business_hours:
+            self.business_hours = self.party.business_hours
+
+    @fields.depends(methods=['set_business_hours'])
     def on_change_party(self):
         super().on_change_party()
-        if self.party:
-            self.business_hours = self.party.business_hours
+        self.set_business_hours()
+
+    @fields.depends(methods=['set_business_hours'])
+    def on_change_shipment_address(self):
+        self.set_business_hours()
