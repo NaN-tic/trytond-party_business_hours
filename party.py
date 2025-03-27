1cb78766e8029e7ff55611a974b58a68c109e003
Bernat Brunet <bernat@nan-tic.com>
2025-03-27 01:05:34 +0100

    Add the hours field in address too, to have the possibility of have differents hours depending on addresses. And set it on the sale.
    Task: #176163

--- party.py
+++ party.py
@@ -7,3 +7,8 @@ from trytond.pool import PoolMeta
 class Party(metaclass=PoolMeta):
     __name__ = 'party.party'
     business_hours = fields.Char('Business Hours')
+
+
+class Address(metaclass=PoolMeta):
+    __name__ = 'party.address'
+    business_hours = fields.Char('Business Hours')
